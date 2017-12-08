e18df2b5391be1a5cdbe8ce9f639f84d07e586b8
fernandp.ext <pablo.fernandez.ext@stago.com>
2017-12-08 19:12:11 +0100

    Core

--- src/Core/DomainModel/User.php
+++ src/Core/DomainModel/User.php
@@ -1,5 +1,4 @@
 <?php
-
 class User{
     
     public $id;
@@ -7,6 +6,27 @@ class User{
     public $password;
     public $passwordValidated;
     
+    public static function fromDataFile($id){
+        $lines = fopen($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        // $lines = array("lol","name","pass","true");
+        
+        $user = new User();
+        $user->id = $lines[0];
+        $user->name = $lines[1];
+        $user->password = $lines[2];
+        $user->passwordValidated = (bool)$lines[3];
+        
+        return $user;
+    }
     
-    
+    public function save(){
+        $fp = fopen($filePaht, 'w');
+        
+        fwrite($fp, $this->id);
+        fwrite($fp, $this->name);
+        fwrite($fp, $this->password);
+        fwrite($fp, $this->passwordValidated);
+        
+        fclose($fp);        
+    }
 }
